4b5100b4f1baafc5fcd75e51715dc0b3b3767678
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-08-12 12:34:58 +0300

    Main Python Program

--- main.py
+++ main.py
@@ -56,3 +56,15 @@ def send_to_chatGPT(messages, model = "gpt-3.5-turbo"):
 
     )
 
+    message = response.choices[0].message.content
+    messages.append(response.choices[0].message)
+    return message
+
+messages = []
+while(1):
+    text = record_text()
+    messages.append({"role": "user", "content": text})
+    response = send_to_chatGPT(messages)
+    SpeakText(response)
+
+
