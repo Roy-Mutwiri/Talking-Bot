742b341db0c6bbb8312904452c91c07cd069a407
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-08-12 12:29:33 +0300

    Main Python Program

--- main.py
+++ main.py
@@ -32,4 +32,27 @@ def record_text():
 
                 audio2 = r.listen(source2)
 
+                MyText = r.recognize_google(audio2)
+
+                return MyText
+
+        except sr.RequestError as e:
+            print(f"Could Not Request Results:  {0}" .format(e))
+
+        except sr.UnknownValueError:
+            print("Unknown Error Occurred")
+
+
+def send_to_chatGPT(messages, model = "gpt-3.5-turbo"):
+
+    response = openai.ChatCompletion.create(
+
+        model=model,
+        messages=messages,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.5
+
+    )
 
