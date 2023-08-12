b64dce980a444985e4c09a5198be300a7495df57
Roy-Mutwiri <mutwiriroy63@gmail.com>
2023-08-12 12:21:41 +0300

    Main Python Program

--- main.py
+++ main.py
@@ -22,3 +22,14 @@ def SpeakText(command):
 r = sr.Recognizer()
 
 def record_text():
+    while(1):
+        try:
+
+            with sr.Microphone() as source2:
+                r.adjust_for_ambient_noise(source2, duration=0.2)
+
+                print("Listening")
+
+                audio2 = r.listen(source2)
+
+
